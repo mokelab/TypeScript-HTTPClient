25bc41e5d4ef5ee40a8d5bb6966f20b76aea9db8
hrk fkm <fkm@mokelab.com>
2017-11-15 07:48:01 +0900

    rebuild

--- xhr/XHRClient.js
+++ xhr/XHRClient.js
@@ -1,3 +1,16 @@
+var Method;
+(function (Method) {
+    Method[Method["GET"] = 0] = "GET";
+    Method[Method["POST"] = 1] = "POST";
+    Method[Method["PUT"] = 2] = "PUT";
+    Method[Method["DELETE"] = 3] = "DELETE";
+})(Method || (Method = {}));
+class HTTPResponse {
+    constructor(status, body) {
+        this.status = status;
+        this.body = body;
+    }
+}
 ///<reference path="../HTTPClient.ts"/>
 class XHRClient {
     constructor(respType) {
